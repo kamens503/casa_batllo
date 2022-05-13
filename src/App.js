5f34849e6d168be82bcfbbb3c192a9cda3076fcf
gustavomarin18 <marinproducciones81@gmail.com>
2022-05-13 16:07:58 -0300

    borre linea logo para probar-marin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-    const lg = "container h-[320px] bg-red-400 grid grid-cols-1 md:grid-cols-2  gap-5"
+  const lg =
+    "container h-[320px] bg-red-400 grid grid-cols-1 md:grid-cols-2  gap-5";
   return (
-    <article id='banner' className='w-[100vw] bg-blue-500 flex justify-center items-center h-[600px]'>
-        <div className={lg}>
-            <div id='ele1' className='w-full h-full bg-green-300 hover:bg-purple-500 transition-all duration-1000'></div>
-            <div id='ele1' className='w-full h-full bg-green-300'></div>
-            <div id='ele1' className='w-full h-full bg-green-300'></div>
-        </div>
+    <article
+      id="banner"
+      className="w-[100vw] bg-blue-500 flex justify-center items-center h-[600px]"
+    >
+      <div className={lg}>
+        <div
+          id="ele1"
+          className="w-full h-full bg-green-300 hover:bg-purple-500 transition-all duration-1000"
+        ></div>
+        <div id="ele1" className="w-full h-full bg-green-300"></div>
+        <div id="ele1" className="w-full h-full bg-green-300"></div>
+      </div>
     </article>
   );
 }
