0c0542c7a926ec374876dcc73ffa969a7513be04
Miguel Nunes <mens793@gmail.com>
2022-05-25 20:43:19 -0300

    Cleaned banner childrens

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import './App.css';
 import './assets/css/index.css';
 import Banner from './components/modules/Banner';
-import Content from './components/utils/Content';
-import Title from './components/utils/Title';
-import banner from './assets/banner_full.jpg';
+import bgBanner from './assets/banner_full.jpg';
 import logoTitle from './assets/logoTitle.svg';
 import Margin from './components/utils/Margin';
-import Gallery from './components/modules/Gallery';
 
 function App() {
 	return (
 		<>
-			<Banner img={banner}>
-				<img src={logoTitle} alt='logo Casa Batlló' className='w-64 m-auto' />
-				<Margin size='xs' />
-				<Title type='banner' tag='h1'>
-					Un lugar agradable y acogedor para tus reuniones
-				</Title>
-				<Margin size='xxs' />
-				<Content className='max-w-lg m-auto'>
-					Donde puedes disfrutar cafe, frappé, smoothie, pizza, crepa y pasta.
-				</Content>
-			</Banner>
-			<Gallery></Gallery>
+			<Banner
+				img={bgBanner}
+				title='Un lugar agradable y acogedor para tus reuniones'
+				subtitle='Donde puedes disfrutar cafe, frappé, smoothie, pizza, crepa y pasta.'
+				logo={logoTitle}
+			></Banner>
 		</>
 	);
 }
--- src/components/modules/Banner.jsx
+++ src/components/modules/Banner.jsx
@@ -1,13 +1,26 @@
-export default function banner(props) {
-	const { img, children } = props;
+import Margin from '../utils/Margin';
+import Title from '../utils/Title';
+import Content from '../utils/Content';
 
+
+
+export default function banner(props) {
+	const { img, title, subtitle, logo } = props;
     const coverAll = 'absolute top-0 left-0 w-full h-full';
 
 	return (
 		<>
 			<section id='banner' className='relative w-full h-[90vh] flex justify-center items-center'>
 				<div id='content-wrapper' className="relative z-20 text-center md:w-1/2">
-                    {children}
+                <img src={logo} alt='logo Casa Batlló' className='w-64 m-auto' />
+                    <Margin size='xs' />
+                    <Title type='banner' tag='h1'>
+                        {title}
+                    </Title>
+                    <Margin size='xxs' />
+                    <Content className='max-w-lg m-auto'>
+                        {subtitle}
+                    </Content>
 				</div>
 				<div
 					id='backdrop'
