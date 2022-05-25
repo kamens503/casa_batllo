a6153325229402adeec7ad9e50754008796a0fdd
Miguel Nunes <mens793@gmail.com>
2022-05-25 13:10:26 -0300

    Added assets, added fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     content: ["./src/**/*.{html,js,jsx}", "./src/**/**/*.{html,js,jsx}"],
     theme: {
+        fontFamily: {
+            'titles' : ['Calistoga', 'cursive'],
+            'content': ['Poppins', 'sans-serif']
+        },
       extend: {},
     },
     plugins: [],
