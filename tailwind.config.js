e6192c0506c8953cb259e35dbc8955b1bc30b315
Miguel Nunes <mens793@gmail.com>
2022-05-25 16:27:53 -0300

    Added background img for banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 module.exports = {
-    content: ["./src/**/*.{html,js,jsx}", "./src/**/**/*.{html,js,jsx}"],
-    theme: {
-        fontFamily: {
-            'title' : ['Calistoga', 'cursive'],
-            'content': ['Poppins', 'sans-serif']
-        },
-      extend: {
-          colors: {
-              'brand' : '#D96006',
-              'brand-yellow' : '#E29524'
-          },
-
-      },
-    },
-    plugins: [],
-  }
+	content: ['./src/**/*.{html,js,jsx}', './src/**/**/*.{html,js,jsx}'],
+	theme: {
+		fontFamily: {
+			title: ['Calistoga', 'cursive'],
+			content: ['Poppins', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				brand: '#D96006',
+				'brand-yellow': '#E29524',
+			},
+			backgroundImage: {
+				'ripped-lg': "url('/src/assets/ripped_lg.svg')",
+				'ripped-xs': "url('/src/assets/ripped_xs.svg')",
+			},
+		},
+	},
+	plugins: [],
+};
