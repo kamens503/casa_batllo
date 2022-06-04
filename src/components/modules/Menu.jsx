268704b04d20a4028cde2b3cd9a176f474dc7204
Miguel Nunes <mens793@gmail.com>
2022-06-04 15:31:17 -0300

    added menu to app

--- src/components/modules/Menu.jsx
+++ src/components/modules/Menu.jsx
@@ -9,18 +9,25 @@ export default function Menu(props) {
 		<>
 			<section
 				id='menu'
-				className='grid grid-cols-1 gap-8 md:gap-16 md:grid-cols-2'
+				className='container grid grid-cols-1 gap-8 m-auto md:gap-16 md:grid-cols-2'
 			>
 				{data.map((menu) => {
 					return (
-						<button className='relative w-full rounded-lg aspect-video group' onClick={openModal}>
-							<Title className='transition-all md:mb-8 md:ml-8 group-hover:opacity-0' tag="h3" type="menu">
+						<button
+							className='relative w-full overflow-hidden rounded-3xl aspect-video group'
+							onClick={openModal}
+						>
+							<Title
+								className='relative z-10 transition-all md:mb-8 md:ml-8 group-hover:opacity-0 md:absolute md:bottom-0 md:text-left'
+								tag='h3'
+								type='menu'
+							>
 								{menu.title}
 							</Title>
 							<img
 								src={menu.img}
 								alt={menu.title}
-								className='w-full h-full transition-all absolute-center opacity-5 group-hover:opacity-100'
+								className='w-full h-full transition-all opacity-60 absolute-center group-hover:opacity-100'
 							/>
 						</button>
 					);
